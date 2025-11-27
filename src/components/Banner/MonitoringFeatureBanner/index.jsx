71f9d2bdf04073385b31c580e30c1cc4d776d567
Gustavo GDF <gustavoaguiar398@gmail.com>
2025-11-27 13:46:40 -0300

    fix: corrigir erro no Link do react router dom

--- src/components/Banner/MonitoringFeatureBanner/index.jsx
+++ src/components/Banner/MonitoringFeatureBanner/index.jsx
@@ -1,11 +1,10 @@
 import styles from './Styles.module.scss'
 import CustomButton from '../../Buttons/CustomButton'
-import { Link } from 'react-scroll'
-import { Link as LinkRouter } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const MonitoringFeatureBanner = () => {
   return (
-    <LinkRouter style={{ textDecoration: 'none' }} to="/monitoring">
+    <Link style={{ textDecoration: 'none' }} to="/monitoring">
       <div className={styles.bannerContainer}>
         <div className={styles.content}>
           <h2>Monitore o preço do seu veículo</h2>
@@ -13,12 +12,10 @@ const MonitoringFeatureBanner = () => {
             Monitore o preço fipe do seu veículo e receba alertas de valorização
             ou desvalorização diretamente no seu celular
           </p>
-          <Link to="consult" smooth={true} offset={-80} duration={500}>
-            <CustomButton size="medium">Ver mais</CustomButton>
-          </Link>
+          <CustomButton size="medium">Ver mais</CustomButton>
         </div>
       </div>
-    </LinkRouter>
+    </Link>
   )
 }
 
