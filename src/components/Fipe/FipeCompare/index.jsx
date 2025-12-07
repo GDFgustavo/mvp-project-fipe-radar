df58b4a8be8d1733b2a2ba3bfb948f830ee057ac
Gustavo GDF <gustavoaguiar398@gmail.com>
2025-12-07 08:47:57 -0300

    feat: adicionar validação ao formulário de comparação

--- src/components/Fipe/FipeCompare/index.jsx
+++ src/components/Fipe/FipeCompare/index.jsx
@@ -2,25 +2,45 @@ import styles from './Styles.module.scss'
 import CustomButton from '../../Buttons/CustomButton'
 import { Link } from 'react-scroll'
 import { AddIcon, RemoveIcon } from '../../SvgIcons'
+import { useState } from 'react'
 
-const FipeCompare = ({ showCompare, toggleCompare, onConsult }) => (
-  <div className={styles.btn}>
-    <CustomButton size="large" onClick={toggleCompare}>
-      {showCompare && <RemoveIcon />}
-      {!showCompare && <AddIcon />}
-      {showCompare ? 'Remover comparação' : 'Adicionar comparação'}
-    </CustomButton>
-    <Link
-      to="result"
-      smooth={true}
-      duration={500}
-      className={styles.reactScrollLink}
-    >
-      <CustomButton size="large" onClick={onConsult}>
-        Consultar
-      </CustomButton>
-    </Link>
-  </div>
-)
+const FipeCompare = ({ showCompare, toggleCompare, onConsult, canCompare }) => {
+  const [compare, setCompare] = useState('')
+
+  const handleCompare = () => {
+    if (!canCompare) {
+      setCompare('Preencha todos os campos acima!.')
+      setTimeout(() => setCompare(''), 4000)
+    } else {
+      toggleCompare()
+    }
+  }
+
+  return (
+    <>
+      <div className={styles.btn}>
+        <CustomButton
+          size="large"
+          onClick={handleCompare}
+          disabled={!canCompare}
+        >
+          {showCompare ? <RemoveIcon /> : <AddIcon />}
+          {showCompare ? 'Remover comparação' : 'Adicionar comparação'}
+        </CustomButton>
+        <Link
+          to="result"
+          smooth={true}
+          duration={500}
+          className={styles.reactScrollLink}
+        >
+          <CustomButton size="large" onClick={onConsult}>
+            Consultar
+          </CustomButton>
+        </Link>
+      </div>
+      {compare && <p className={styles.msg}>{compare}</p>}
+    </>
+  )
+}
 
 export default FipeCompare
